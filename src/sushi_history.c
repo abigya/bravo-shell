e9217b28d2a8aba141a3065c5159d21f6ed34611
Abigya Devkota <37885957+abigya@users.noreply.github.com>
2019-01-30 23:58:53 -0600

    Update sushi_history.c

--- src/sushi_history.c
+++ src/sushi_history.c
@@ -1,27 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "sushi.h"
 
+static int count = 0;
 static char *history[SUSHI_HISTORY_LENGTH] = {NULL};
-static int i = 0;
 
 void sushi_store(char *line){
-   //make sure line is not NULL
-    if (line!=NULL){
-      history[i] = malloc((SUSHI_MAX_INPUT+1));
-      strncpy(history[i],line,SUSHI_MAX_INPUT);
-      i++;
+ 
+   int hist =  SUSHI_HISTORY_LENGTH;
+  //if history is not empty shift elements to right by 1
+  if (count<hist && count!=0){
+    for(int i = count+1;i>0;i--){
+      history[i]=history[i-1];
     }
+  }
+   
+   if (line!=NULL){
+      history[0] = malloc((strlen(line)+1));
+      strncpy(history[0],line,strlen(line)+1);
+      count++;
+    }
+  
+
     //check if history is full and remove recently inserted string
-    int hist =  SUSHI_HISTORY_LENGTH;
-    if (i>(hist-1)){
-      strncpy(history[i],"",SUSHI_MAX_INPUT);
-      free(history[i]);
+    
+    if (count>(hist-1)){
+      fprintf(stdout,"history is full!\n");
+      free(history[0]);
     }
+   
  }
 
 void sushi_show_history() {
-    for(int j=0;j<i;j++){
-        fprintf(stdout,"%d   %s",j+1,history[j]);
+    for(int j=0;j<count;j++){
+        fprintf(stdout,"%d   %s\n",j+1,history[j]);
     }
 }
+
