5b00b578dec39907cb9fc3f127ca9ca79ae06e25
Abigya Devkota <37885957+abigya@users.noreply.github.com>
2019-02-01 12:38:50 -0600

    Update sushi_read.c

--- src/sushi_read.c
+++ src/sushi_read.c
@@ -4,16 +4,31 @@
 #include <string.h>
 #include <ctype.h>
 
+
+//strlcpy is not provided on windows so i created a function
+//credits : stackoverflow
+void strlcpy(char* dst, const char* src, size_t bufsize){
+  size_t srclen =strlen(src);
+  size_t result =srclen; /* Result is always the length of the src string */
+  if(bufsize>0)
+  {
+    if(srclen>=bufsize)
+       srclen=bufsize-1;
+    if(srclen>0)
+       memcpy(dst,src,srclen);
+    dst[srclen]='\0';
+  }
+}
 char *sushi_read_line(FILE *in) {
   //create buffer for line
   char *buffer;
   buffer = (char*)malloc(sizeof(char)*SUSHI_MAX_INPUT+1);
   ssize_t line; //number of characters read by the buffer
   size_t len = SUSHI_MAX_INPUT; // number of bytes in the read buffer    
-  line = getline(&buffer, &len, in);
+  line = getline(&buffer, &len, in); // check this line 
   
   //check for errors in I/O
-  if (line==-1){
+  if (line ==-1){ //check this line 
     perror("There is some error with the input!\n");
     return NULL;
   }
@@ -24,21 +39,25 @@ char *sushi_read_line(FILE *in) {
 
   //check length of line 
   char *string;
+  size_t length;
   if (line>SUSHI_MAX_INPUT){
       fprintf(stderr,"Line is too long,truncated\n");
+      length = SUSHI_MAX_INPUT;
       string = (char*)malloc(sizeof(char)*(SUSHI_MAX_INPUT+1));
-      strncpy(string,buffer,SUSHI_MAX_INPUT);
-      string[strlen(string)]='\0';
+
+      strlcpy(string,buffer,length);
+      return string;
 
     }else{
       buffer[strcspn(buffer, "\n")] = 0;
+       length = line;
       string = (char*)malloc(sizeof(char)*(line+1));
-      strncpy(string,buffer,line);
+      strlcpy(string,buffer,length);
+      return string;
    
   }
-  return string;
+ 
 }
-
  
 int sushi_read_config(char *fname) {
   FILE *infile;
@@ -49,11 +68,10 @@ int sushi_read_config(char *fname) {
     char *line;
     while (!feof(infile)){
       line = sushi_read_line(infile);
+      sushi_store(line);
     }
 
     fclose(infile);
 
   return 0;
 }
-
- 
